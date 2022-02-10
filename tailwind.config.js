9527c72726815368cc0560c1081dcfda4081fdf9
Sabir Khan <76485932+simplysabir@users.noreply.github.com>
2022-02-10 21:33:02 +0530

    Home Page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+   
+    extend: {
+      fontFamily: {
+        'pop': 'Poppins'
+      },
+      colors: {
+        code: {
+          'darkblue':"#05386B",
+          'darkgreen':"#379683",
+          'bggreen':" #5CDB95",
+          'lightgreen':"#8EE4AF",
+          'codewhite':"#EDF5E1"
+  
+        },
+      }
+    },
+  },
+  plugins: [],
+}
