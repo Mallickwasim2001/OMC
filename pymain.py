12984f58b520256b13c151e30cf60e11ef15e54c
Wasimuddin Zakirhussain Mallick <89626037+Mallickwasim2001@users.noreply.github.com>
2022-04-26 17:18:51 +0530

    Add files via upload
    NEW AND UPDATED

--- pymain.py
+++ pymain.py
@@ -10,7 +10,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'users'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-
 """conn = connector.connect(host="localhost",
                          user="root",
                          password="",
@@ -20,9 +19,9 @@ mysql.init_app(app)
 #mysql = MySQL(app)
 
 
-@app.route("/")
+@app.route("/index")
 def home():
-    return render_template('loginp.html')
+    return render_template('index.html')
 
 
 @app.route("/login")
@@ -40,7 +39,9 @@ def signup():
         confirmpassword = request.form['cpassword']
         """cursor = conn.cursor()"""
         cursor = mysql.connect().cursor()
-        cursor.execute(f"insert into users values('{firstname}','{lastname}','{email}','{password}','{confirmpassword}')")
+        cursor.execute(
+            f"insert into users values('{firstname}','{lastname}','{email}','{password}','{confirmpassword}')"
+        )
         #mysql.connect().commit()
         cursor.connection.commit()
         cursor.close()
@@ -56,7 +57,9 @@ def login_validation():
         email = request.form.get('email')
         password = request.form.get('password')
         cursor = mysql.connect().cursor()
-        cursor.execute(" SELECT * FROM users WHERE email LIKE '{}' AND password LIKE '{}' ".format(email, password))
+        cursor.execute(
+            " SELECT * FROM users WHERE email LIKE '{}' AND password LIKE '{}' "
+            .format(email, password))
         users = cursor.fetchall()
         print(users)
         if not users:
@@ -68,11 +71,13 @@ def login_validation():
     else:
         return render_template('loginp.html')
 
+
 @app.route("/forgotpasswd")
 def forgotpasswd():
     return render_template('forgotpassP.html')
 
-@app.route("/email_validation" , methods=['POST'])
+
+@app.route("/email_validation", methods=['POST'])
 def email_valid():
     if request.method == "POST":
         email = request.form.get('email')
@@ -80,24 +85,25 @@ def email_valid():
         cursor.execute(" select email from users where 1 ".format(email))
         users = cursor.fetchall()
         print(users)
-        if not users :
+        if not users:
             cursor.close()
             return render_template('signupP.html')
         else:
             #return "Logged in successfully"
             return render_template('passwd_reset.html')
-    
 
-@app.route("/passwd_reset" , methods=['POST', 'GET'])
+
+@app.route("/passwd_reset", methods=['POST', 'GET'])
 def passwd_reset():
     if request.method == "POST":
-        
+
         password = request.form['password']
         confirmpassword = request.form['cpassword']
         """cursor = conn.cursor()"""
         cursor = mysql.connect().cursor()
-        cursor.execute(" UPDATE `users` SET `password`='4321',`cpassword`='4321' WHERE 1")
-        
+        cursor.execute(
+            " UPDATE `users` SET `password`='4268',`cpassword`='4268' WHERE 1")
+
         mysql.connect().commit()
         #mysql.connect().commit()
         cursor.connection.commit()
@@ -121,14 +127,141 @@ def add_user():
         password = request.form['password']
         confirmpassword = request.form['cpassword']
         cursor = mysql.connect().cursor()
-        cursor.execute(''' INSERT INTO `users` (`firstname`,`lastname`,`email`,`password`,`confirmpassword`)
-        5 VALUES(`{}`,`{}`,`{}`,`{}`,`{}`)'''.format(firstname, lastname, email, password, confirmpassword))
+        cursor.execute(
+            ''' INSERT INTO `users` (`firstname`,`lastname`,`email`,`password`,`confirmpassword`)
+        5 VALUES(`{}`,`{}`,`{}`,`{}`,`{}`)'''.format(firstname, lastname,
+                                                     email, password,
+                                                     confirmpassword))
         mysql.connect().commit()
         cursor.close()
         return f"Done!!"
     else:
         return render_template('loginp.html')
 
+
 # add doctor
+@app.route("/signupd", methods=['POST', 'get'])
+def signupd():
+    if request.method == "POST":
+        firstname = request.form['fname']
+        lastname = request.form['lname']
+        email = request.form['email']
+        password = request.form['password']
+        confirmpassword = request.form['cpassword']
+        """cursor = conn.cursor()"""
+        cursor = mysql.connect().cursor()
+        cursor.execute(
+            f"insert into docusers values('{firstname}','{lastname}','{email}','{password}','{confirmpassword}')"
+        )
+        #mysql.connect().commit()
+        cursor.connection.commit()
+        cursor.close()
+        #mysql.connect().cursor()
+        return render_template('logind.html')
+    else:
+        return render_template('signupd.html')
+
+
+@app.route("/logind")
+def logind():
+    return render_template('logind.html')
+
+
+@app.route('/login_validation_doc', methods=['POST'])
+def login_validation_doc():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        cursor = mysql.connect().cursor()
+        cursor.execute(
+            " SELECT * FROM docusers WHERE email LIKE '{}' AND password LIKE '{}' "
+            .format(email, password))
+        users = cursor.fetchall()
+        print(users)
+        if not users:
+            cursor.close()
+            return "Username or Password is wrong"
+        else:
+            #return "Logged in successfully"
+            return render_template('doctorlanding.html')
+    else:
+        return render_template('logind.html')
+
+
+app.route('/add_user_doc', methods=['POST', 'GET'])
+
+
+def add_user_doc():
+
+    if request.method == 'POST':
+        firstname = request.form['fname']
+        lastname = request.form['lname']
+        email = request.form['email']
+        password = request.form['password']
+        confirmpassword = request.form['cpassword']
+        cursor = mysql.connect().cursor()
+        cursor.execute(
+            f"insert into docusers values('{firstname}','{lastname}','{email}','{password}','{confirmpassword}')"
+        )
+        mysql.connect().commit()
+        cursor.close()
+        return f"Done!!"
+    else:
+        return render_template('logind.html')
+
+
+#contact us
+@app.route("/contactus", methods=['POST','GET'])
+def contactus():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        cursor = mysql.connect().cursor()
+        cursor.execute(
+            f"insert into contactus values('{name}','{email}','{message}')"
+        )
+
+        cursor.connection.commit()
+        cursor.close()
+
+        #cursor.execute(
+        #    f"insert into docusers values('{name}','{email}','{message}')"
+        #)
+        return f"done!!"
+    else:
+        return render_template('contact.html')
+
+@app.route("/about")
+def home1():
+    return render_template('about.html')
+
+@app.route("/services")
+def home2():
+    return render_template('services.html')
+
+@app.route("/contact")
+def home3():
+    return render_template('contact.html')
+
+@app.route("/covid")
+def home4():
+    return render_template('covid.html')
+
+@app.route("/covidlandig")
+def home5():
+    return render_template('covidlandig.html')
+
+@app.route("/doctorlanding")
+def home6():
+    return render_template('doctorlanding.html')
+
+@app.route("/general")
+def home7():
+    return render_template('general.html')
+
+
+
 
 app.run(debug=True)
